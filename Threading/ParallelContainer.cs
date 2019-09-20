822b875e3481097f232c5449488717c502ecc3a1
Vladimir Ryavkin <vryavkin@outlook.com>
2019-09-20 19:37:55 +0300

    First commit

---
+++ Threading/ParallelContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Threading
+{
+    public class ParallelContainer<T>
+    {
+        public List<T> RunParallely(List<Func<T>> functions)
+        {
+            var count = functions.Count;
+            var result = new List<T>();
+            var threads = new List<Thread>();
+            var counter = new CountdownEvent(count);
+
+            foreach (var func in functions)
+            {
+                var copy = func;
+                threads.Add(new Thread(() => {
+                    result.Add(copy());
+                    counter.Signal();
+                }));
+            }
+            foreach (var t in threads)
+            {
+                t.Start();
+            }
+
+            counter.Wait();
+
+            return result;
+        }
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Linq;
+
+namespace Threading
+{
+
+    class Program
+    {
+
+        public static string Shit { get; set; }
+        static void Main(string[] args)
+        {
+            var list = new List<string>();
+            new List<Func<string>> {
+                DoFacebook,
+                DoYoutube,
+                DoSlutstagram,
+                DoTwitter
+            }.AsParallel().ForAll(x => list.Add(x()));
+
+            Console.ReadLine();
+        }
+
+        private static void Container()
+        {
+            var container = new ParallelContainer<string>();
+            var result = container.RunParallely(new List<Func<string>> {
+                DoFacebook,
+                DoYoutube,
+                DoSlutstagram,
+                DoTwitter
+            });
+
+            Console.WriteLine($"List has {result.Count} elements");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void Threads()
+        {
+            var countdown = new CountdownEvent(4);
+
+            var list = new List<string>();
+
+            var twThread = new Thread(() =>
+            {
+                list.Add(DoTwitter());
+                countdown.Signal();
+            });
+
+            var fbThread = new Thread(() =>
+            {
+                list.Add(DoFacebook());
+                countdown.Signal();
+
+            });
+
+
+            var ytThread = new Thread(() =>
+            {
+                list.Add(DoYoutube());
+                countdown.Signal();
+
+            });
+
+            var slThread = new Thread(() =>
+            {
+                list.Add(DoSlutstagram());
+                countdown.Signal();
+            });
+
+            twThread.Start();
+            fbThread.Start();
+            ytThread.Start();
+            slThread.Start();
+
+            countdown.Wait();
+            // Wait here
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+        static string DoTwitter()
+        {
+            Thread.Sleep(new Random().Next(2000, 4000));
+            Console.WriteLine("Twitter complete");
+            return "Twitter post";
+        }
+        static string DoFacebook()
+        {
+            Thread.Sleep(new Random().Next(2000, 4000));
+            Console.WriteLine("Fb complete");
+            return "Facebook post";
+        }
+
+        static string DoYoutube()
+        {
+            Thread.Sleep(new Random().Next(2000, 4000));
+            Console.WriteLine("YT complete");
+            return "Youtube post";
+        }
+
+        static string DoSlutstagram()
+        {
+            Thread.Sleep(new Random().Next(2000, 4000));
+            Console.WriteLine("SS complete");
+            return "Slut post";
+        }
+    }
+}
