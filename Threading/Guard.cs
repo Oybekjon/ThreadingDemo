4a3c44b4699ca0e7234cac309ccef3d9193188ad
Oybek <jumaniyozov@oybek.com>
2019-10-25 11:54:54 +0300

    Guard added

---
+++ Threading/Guard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threading
+{
+    public static class Guard
+    {
+        public static void NotNull(object value, string name)
+        {
+            if (value == null)
+                throw new ArgumentNullException(name);
+        }
+    }
+}
