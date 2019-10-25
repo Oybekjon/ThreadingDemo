19f5485709fb6b69e13f10f81c4d32221749b238
Oybek <jumaniyozov@oybek.com>
2019-10-25 11:57:26 +0300

    Timer extensions

---
+++ Threading/TimerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Threading
+{
+    public static class TimerExtensions
+    {
+        public static void Start(this Timer timer)
+        {
+            if (timer == null)
+                throw new ArgumentNullException(nameof(timer));
+        }
+    }
+}
